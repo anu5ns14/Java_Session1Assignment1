b48340ba74e9f816a168190abc716b63689aef8c
Anurag Choudhary <anu5ns14@gmail.com>
2016-08-08 00:02:48 +0530

    Sum of two numbers

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,11 @@
+
+public class SumOfTwoNumbers {
+	public static void main(String[] args) {
+		int numOne = 10;
+		float numTwo = 20;
+		
+		int sum = (int) (numOne + numTwo);
+		
+		System.out.println("The sum of two numbers is: "+sum);
+	}
+}
